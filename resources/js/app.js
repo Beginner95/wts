8c78cc5a32fe1379914dfa8f4c6ea8475ad869b6
Vaharsolta <vaha-22@mail.ru>
2020-03-18 17:01:20 +0300

    Added js

--- resources/js/app.js
+++ resources/js/app.js
@@ -30,3 +30,106 @@ Vue.component('example-component', require('./components/ExampleComponent.vue').
 const app = new Vue({
     el: '#app',
 });
+
+
+$(document).on('click', '.sidebar-steps_list-link', function(event){
+    event.preventDefault();
+    $('html, body').animate({
+        scrollTop: $( $.attr(this, 'href') ).offset().top - 100
+    }, 700);
+    $('.sidebar-steps_list-link').removeClass('active');
+});
+
+function clickHandler(e) {
+    if (e.target.classList.contains('nav_btn')) {
+        e.target.classList.toggle('active');
+        document.querySelector('nav').classList.toggle('active');
+    }
+    if (e.target.classList.contains('services_item-toggler')) {
+        e.target.parentNode.parentNode.classList.toggle('services_item-active');
+    }
+    if (e.target.classList.contains('show_modal')) {
+        document.querySelector('.modal').classList.add('modal-active');
+    }
+    if (e.target.classList.contains('hide_modal')) {
+        document.querySelector('.modal-active').classList.remove('modal-active');
+    }
+    if (e.target.classList.contains('filter_toggler')) {
+        if (e.target.innerText == 'Фильтрация') {
+            e.target.innerText = 'Скрыть';
+        }
+        else if (e.target.innerText == 'Скрыть') {
+            e.target.innerText = 'Фильтрация';
+        }
+        document.querySelector('.filters_wrap').classList.toggle('filters_wrap-active');
+    }
+    if (e.target.classList.contains('radio_bg') && e.target.previousElementSibling.value == 'dark_bg') {
+        document.querySelector('.post_content').classList.add('dark_mode');
+    }
+    if (e.target.classList.contains('radio_bg') && e.target.previousElementSibling.value == 'white_bg') {
+        document.querySelector('.post_content').classList.remove('dark_mode');
+    }
+    if (e.target.classList.contains('sidebar-steps_list-link')) {
+
+        e.target.classList.add('active');
+    }
+    if (e.target.classList.contains('show_text')) {
+        if (e.target.previousElementSibling.classList.contains('text_block-showed')) {
+            e.target.innerText = 'Развернуть текст';
+            e.target.previousElementSibling.classList.remove('text_block-showed')
+        }
+        else {
+            e.target.innerText = 'Свернуть текст';
+            e.target.previousElementSibling.classList.add('text_block-showed');
+            var timeout = setTimeout(function (){
+                e.target.previousElementSibling.style.maxHeight = 'auto';
+            },500)
+        }
+    }
+    if (e.target.classList.contains('show_second_lvl')) {
+        e.target.nextElementSibling.classList.toggle('active');
+        e.target.classList.toggle('active');
+    }
+}
+
+document.addEventListener('click', clickHandler);
+
+function scrollHandler(e) {
+    if (document.querySelector('.trigger')) {
+        var trigger = document.querySelector('.trigger');
+        var fixer = document.querySelector('.target');
+        if (trigger.getBoundingClientRect().top < 0) {
+            var heightBlock = window.getComputedStyle(fixer).height;
+            var heightWrap = window.getComputedStyle(trigger).height;
+            var distanceToTop = trigger.getBoundingClientRect().top * -1 + 150;
+            var distanceToBottom = parseInt(heightWrap) - parseInt(heightBlock) - parseInt(distanceToTop);
+            if (distanceToBottom <= 50 ) {
+                fixer.style.top = 'unset';
+                fixer.style.bottom = '50px';
+            }
+            else {
+                fixer.style.top = distanceToTop + 'px'; // задаем положение блок сверху
+                fixer.style.bottom = 'unset';
+            }
+
+        }
+        else {
+            fixer.style.top = 0;
+        }
+    }
+
+    document.querySelectorAll('.section_name-step').forEach(function(item){
+        if (item.getBoundingClientRect().top <= 150 ) {
+            document.querySelectorAll('.sidebar-steps_list-link').forEach(function(link) {
+                link.classList.remove('active');
+                if (link.getAttribute('href').slice(1) == item.id) {
+                    link.classList.add('active');
+                }
+            })
+        } else {
+
+        }
+    })
+}
+
+document.addEventListener('scroll', scrollHandler);
