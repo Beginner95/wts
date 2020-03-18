de7fc53b60f8166736abde08298ed61e5347e58c
Vaharsolta <vaha-22@mail.ru>
2020-03-18 17:41:11 +0300

    Installed laravel-menu

--- config/app.php
+++ config/app.php
@@ -174,6 +174,7 @@ return [
         // App\Providers\BroadcastServiceProvider::class,
         App\Providers\EventServiceProvider::class,
         App\Providers\RouteServiceProvider::class,
+        Lavary\Menu\ServiceProvider::class,
 
     ],
 
@@ -225,6 +226,7 @@ return [
         'URL' => Illuminate\Support\Facades\URL::class,
         'Validator' => Illuminate\Support\Facades\Validator::class,
         'View' => Illuminate\Support\Facades\View::class,
+        'Menu'      => Lavary\Menu\Facade::class,
 
     ],
 
