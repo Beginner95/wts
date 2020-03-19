9614d987f96112e63605950ddf947e57e2a0e6f5
Vaharsolta <vaha-22@mail.ru>
2020-03-19 20:29:00 +0300

    Added the ability to sort and specify the limit

--- app/Repositories/Repository.php
+++ app/Repositories/Repository.php
@@ -2,14 +2,23 @@
 
 namespace App\Repositories;
 
-use Illuminate\Support\Facades\Config;
-
 abstract class Repository
 {
     protected $model = false;
 
-    public function get()
+    public function get($select = '*', $orderBy = false, $take = false)
     {
-        return $this->model->get();
+        $builder = $this->model->select($select);
+
+        if ($orderBy) {
+            list($column, $val) = explode(',', $orderBy);
+            $builder->orderBy($column, $val);
+        }
+
+        if ($take) {
+            $builder->take($take);
+        }
+
+        return $builder->get();
     }
 }
