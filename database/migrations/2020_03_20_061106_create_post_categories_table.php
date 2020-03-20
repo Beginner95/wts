fe3c5abc368035260ef89a47780300a7a94b5ff3
Vaharsolta <vaha-22@mail.ru>
2020-03-20 09:51:38 +0300

    Added post categories migration

---
+++ database/migrations/2020_03_20_061106_create_post_categories_table.php
@@ -0,0 +1,35 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreatePostCategoriesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('post_categories', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('category');
+            $table->string('slug');
+            $table->string('title')->nullable();
+            $table->string('description')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('post_categories');
+    }
+}
