11c9bbe9d37dc278256e1ccc31790b971b7354b2
Vaharsolta <vaha-22@mail.ru>
2020-03-21 12:01:55 +0300

    Created template for footer

---
+++ resources/views/footer.blade.php
@@ -0,0 +1,27 @@
+<!-- footer -->
+<footer class="footer center">
+    <div class="footer_contacts">
+        <a href="#" class="btn btn-bordered">info@waytostart.ru</a>
+        <a href="#" class="btn btn-bordered address_link">Москва, Цветной бульвар, 7-11</a>
+        <a href="#" class="btn btn-filled">+7 (499) 372 - 27 - 02</a>
+        <a href="#" class="btn btn-bordered">IG</a>
+        <a href="#" class="btn btn-bordered">FB</a>
+        <a href="#" class="btn btn-bordered">VK</a>
+        <a href="#" class="btn btn-bordered">TG</a>
+        <a href="#" class="btn btn-bordered">WA</a>
+        <a href="#" class="btn btn-bordered">ZEN</a>
+    </div>
+
+    <ul class="footer_nav d-flex">
+        <li><a href="#">Портфолио</a></li>
+        <li><a href="#">Услуги</a></li>
+        <li><a href="#">О нас</a></li>
+        <li><a href="#">Блог</a></li>
+        <li><a href="#">Отзывы</a></li>
+        <li><a href="#">Вакансии</a></li>
+        <li><a href="#">Контакты</a></li>
+    </ul>
+    <a href="#" class="btn btn-bordered goTop">↑ Наверх</a>
+    <div class="copyright">WAYTOSTART.RU • {{ date('Y') }}</div>
+</footer>
+<!-- footer -->
--- resources/views/layouts/site.blade.php
+++ resources/views/layouts/site.blade.php
@@ -21,73 +21,12 @@
 </head>
 <body>
 
-<!-- header -->
+
 @yield('header')
-<!-- header -->
+
 @yield('content')
 
-<!-- footer -->
-<footer class="footer center">
-    <div class="footer_contacts">
-        <a href="#" class="btn btn-bordered">
-            info@waytostart.ru
-        </a>
-        <a href="#" class="btn btn-bordered address_link">
-            Москва, Цветной бульвар, 7-11
-        </a>
-        <a href="#" class="btn btn-filled">
-            +7 (499) 372 - 27 - 02
-        </a>
-        <a href="#" class="btn btn-bordered">
-            IG
-        </a>
-        <a href="#" class="btn btn-bordered">
-            FB
-        </a>
-        <a href="#" class="btn btn-bordered">
-            VK
-        </a>
-        <a href="#" class="btn btn-bordered">
-            TG
-        </a>
-        <a href="#" class="btn btn-bordered">
-            WA
-        </a>
-        <a href="#" class="btn btn-bordered">
-            ZEN
-        </a>
-    </div>
-    <ul class="footer_nav d-flex">
-        <li><a href="#">
-                Портфолио
-            </a></li>
-        <li><a href="#">
-                Услуги
-            </a></li>
-        <li><a href="#">
-                О нас
-            </a></li>
-        <li><a href="#">
-                Блог
-            </a></li>
-        <li><a href="#">
-                Отзывы
-            </a></li>
-        <li><a href="#">
-                Вакансии
-            </a></li>
-        <li><a href="#">
-                Контакты
-            </a></li>
-    </ul>
-    <a href="#" class="btn btn-bordered goTop">
-        ↑ Наверх
-    </a>
-    <div class="copyright">
-        WAYTOSTART.RU • 2020
-    </div>
-</footer>
-<!-- footer -->
+@include('footer')
 
 <!-- modals -->
 <div class="modal center">
@@ -117,12 +56,6 @@
 					Телефон
 				</span>
         </label>
-        <!-- <label class="input_wrap">
-            <input type="email" name="email" class="input">
-            <span class="input_label">
-                Почта
-            </span>
-        </label> -->
         <label class="input_wrap input_wrap-wide">
             <textarea rows="3" name="comment" class="input"></textarea>
             <span class="input_label">
