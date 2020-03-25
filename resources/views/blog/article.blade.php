4546be62f087fa7c0fece474654ec3f46e476b36
Vaharsolta <vaha-22@mail.ru>
2020-03-25 14:15:00 +0300

    Added links to share

--- resources/views/blog/article.blade.php
+++ resources/views/blog/article.blade.php
@@ -35,6 +35,24 @@
                         <span class="share_counter">{{ $article->claps }}</span>
                     </button>
                 </li>
+                <li>
+                    <a href="https://vk.com/share.php?url={{ route('blog.show', ['slug' => $article->slug] )}}&title={{ $article->title }}&description={{ $article->short_desc }}&image={{route('home')}}/images/{{ $article->main_cover }}" target="_blank">
+                        <button class="share_btn">
+                            <svg width="20" height="9" viewBox="0 0 20 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M10.1024 0.599999L6.50242 9H4.16242L0.574422 0.599999H3.14242L5.42242 6.072L7.75042 0.599999H10.1024ZM14.0741 5.952L13.1861 6.9V9H10.8341V0.599999H13.1861V4.092L16.4381 0.599999H19.0541L15.6221 4.32L19.2341 9H16.4741L14.0741 5.952Z" fill="#E5E5E5"/>
+                            </svg>
+                        </button>
+                    </a>
+                </li>
+                <li>
+                    <a href="https://www.facebook.com/sharer/sharer.php?u={{ route('blog.show', ['slug' => $article->slug]) }}" target="_blank">
+                        <button class="share_btn">
+                            <svg width="17" height="9" viewBox="0 0 17 9" fill="none" xmlns="http://www.w3.org/2000/svg">
+                                <path d="M2.51506 2.436V4.284H6.22306V6.12H2.51506V9H0.139063V0.599999H6.72706V2.436H2.51506ZM14.5343 4.62C15.0143 4.788 15.3903 5.052 15.6623 5.412C15.9343 5.764 16.0703 6.192 16.0703 6.696C16.0703 7.432 15.7743 8 15.1823 8.4C14.5903 8.8 13.7343 9 12.6143 9H8.07828V0.599999H12.3743C13.4463 0.599999 14.2623 0.799999 14.8223 1.2C15.3823 1.592 15.6623 2.124 15.6623 2.796C15.6623 3.196 15.5623 3.556 15.3623 3.876C15.1703 4.188 14.8943 4.436 14.5343 4.62ZM10.4303 2.316V3.936H12.0623C12.8623 3.936 13.2623 3.664 13.2623 3.12C13.2623 2.584 12.8623 2.316 12.0623 2.316H10.4303ZM12.4223 7.284C13.2543 7.284 13.6703 7 13.6703 6.432C13.6703 5.864 13.2543 5.58 12.4223 5.58H10.4303V7.284H12.4223Z" fill="#E5E5E5"/>
+                            </svg>
+                        </button>
+                    </a>
+                </li>
             </ul>
         </aside>
         <article class='text_block'>
