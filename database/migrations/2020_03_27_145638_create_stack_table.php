2e5b59f0ddd257a9d3a1a3c0bce8cbd9ec11a346
Vaharsolta <vaha-22@mail.ru>
2020-03-27 18:06:25 +0300

    Added migration stack

---
+++ database/migrations/2020_03_27_145638_create_stack_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Support\Facades\Schema;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Database\Migrations\Migration;
+
+class CreateStackTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('stack', function (Blueprint $table) {
+            $table->bigIncrements('id');
+            $table->string('name');
+            $table->string('icon')->nullable();
+            $table->string('backend')->nullable();
+            $table->string('frontend')->nullable();
+            $table->string('framework')->nullable();
+            $table->string('cms')->nullable();
+            $table->string('mobile')->nullable();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('stack');
+    }
+}
