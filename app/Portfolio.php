07b6f2ef3f3806e7ddb8408f58b27b00b0ca6f01
Vaharsolta <vaha-22@mail.ru>
2020-03-27 17:38:29 +0300

    Added method belongs to many project category

--- app/Portfolio.php
+++ app/Portfolio.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class Portfolio extends Model
 {
-    //
+    public function categories()
+    {
+        return $this->belongsToMany(ProjectCategory::class);
+    }
 }
