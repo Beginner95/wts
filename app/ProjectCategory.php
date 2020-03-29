3e307654d18d380ec861c282579d229fa92ae1ad
Vaharsolta <vaha-22@mail.ru>
2020-03-29 18:57:08 +0300

    Added method projects many to many

--- app/ProjectCategory.php
+++ app/ProjectCategory.php
@@ -6,5 +6,8 @@ use Illuminate\Database\Eloquent\Model;
 
 class ProjectCategory extends Model
 {
-    //
+    public function projects()
+    {
+        return $this->belongsToMany(Portfolio::class);
+    }
 }
